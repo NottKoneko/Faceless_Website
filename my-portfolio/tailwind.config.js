81581aba7ccf1ef81ebfe904338d43b4423fbcc6
Koneko <pumasmichael@yahoo.com>
2024-11-18 16:37:02 +0000

    addec theme tailwind

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#000000',
+        accent: '#6943FF',
+        softPink: '#FF7EB6',
+        textPrimary: '#E0E0E0',
+        textSecondary: '#A5A5A5',
+        highlight: '#FFD700',
+        error: '#FF4F4F',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        pacifico: ['Pacifico', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
