dcb1d0414c84d839533d95548f8874c1a97c9412
Koneko <pumasmichael@yahoo.com>
2024-11-18 10:49:53 +0000

    ret

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaEnvelope } from 'react-icons/fa';
 
 const About = () => {
   return (
